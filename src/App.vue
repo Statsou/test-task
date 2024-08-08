76aba79a5765acc94d95ed856cf0992542d90fcb
Ник <110625824+Statsou@users.noreply.github.com>
2024-08-08 15:40:42 +0300

    fixed state isLoggedIn when the page is refreshed

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,19 @@
 <script>
 import THeader from '@/components/Header.vue';
 import TFooter from '@/components/Footer.vue';
-
+import { actionTypes } from '@/store/modules/auth';
 
 export default {
   name: 'TApp',
   components: {
     THeader,
     TFooter
+  },
+  mounted() {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      this.$store.dispatch(actionTypes.login)
+    }
   }
 }
 </script>
